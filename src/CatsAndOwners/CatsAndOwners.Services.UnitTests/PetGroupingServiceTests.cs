cbd8e6f24a3c8e56bf9c4980b74fb82670c93223
John Downs <john.downs@kloud.com.au>
2017-08-24 08:23:41 +1000

    Add initial shell test cases

--- src/CatsAndOwners/CatsAndOwners.Services.UnitTests/PetGroupingServiceTests.cs
+++ src/CatsAndOwners/CatsAndOwners.Services.UnitTests/PetGroupingServiceTests.cs
@@ -5,6 +5,40 @@ namespace CatsAndOwners.Services.UnitTests
     [TestClass]
     public class PetGroupingServiceTests
     {
+        [TestMethod]
+        public void GetPetNamesByOwner_WithSingleOwnerAndPet_ReturnsGenderAndPetName()
+        {
+            
+        }
 
+        [TestMethod]
+        public void GetPetNamesByOwner_WithMultipleOwnersAndSinglePet_ReturnsGendersAndPetName()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPetNamesByOwner_WithMultipleOwnersAndMultiplePets_ReturnsGendersAndPetName()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPetNamesByOwner_WithSingleOwnerWithNullPetName_ReturnsEmptyDictionary()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPetNamesByOwner_WithNullOwnersList_ReturnsEmptyDictionary()
+        {
+
+        }
+
+        [TestMethod]
+        public void GetPetNamesByOwner_WithEmptyOwnersList_ReturnsEmptyDictionary()
+        {
+            
+        }
     }
 }
