beb9b36275119b25b06c0cbb4dc2a1c124c7bd5b
John Downs <john.downs@kloud.com.au>
2017-08-24 09:01:59 +1000

    Implement core grouping logic

--- src/CatsAndOwners/CatsAndOwners.Services/PetGroupingService.cs
+++ src/CatsAndOwners/CatsAndOwners.Services/PetGroupingService.cs
@@ -16,8 +16,18 @@ namespace CatsAndOwners.Services
                 return new Dictionary<Gender, List<string>>();
             }
 
-            // TODO
-            throw new System.NotImplementedException();
+            // perform a grouping by gender, and return back just the pet names
+            return owners
+                .GroupBy(o => new { o.Gender })
+                .Select(s => new
+                {
+                    OwnerGender = s.Key.Gender,
+                    PetNames = s
+                        .SelectMany(p => p.Pets)
+                        .Select(p => p.Name)
+                        .ToList()
+                })
+                .ToDictionary(keySelector: g => g.OwnerGender, elementSelector: g => g.PetNames);
         }
     }
 }
