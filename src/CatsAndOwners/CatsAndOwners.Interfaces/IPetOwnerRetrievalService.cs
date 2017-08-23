3e432b6cb6fd4fdc761973870164c2da210fc439
John Downs <john.downs@kloud.com.au>
2017-08-24 09:14:34 +1000

    Add PetOwnerRetrievalService

---
+++ src/CatsAndOwners/CatsAndOwners.Interfaces/IPetOwnerRetrievalService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using CatsAndOwners.Models;
+
+namespace CatsAndOwners.Interfaces
+{
+    public interface IPetOwnerRetrievalService
+    {
+        IList<Owner> GetPetsOwners();
+    }
+}
---
+++ src/CatsAndOwners/CatsAndOwners.Services/PetOwnerRetrievalService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using CatsAndOwners.Interfaces;
+using CatsAndOwners.Models;
+
+namespace CatsAndOwners.Services
+{
+    public class PetOwnerRetrievalService : IPetOwnerRetrievalService
+    {
+        public IList<Owner> GetPetsOwners()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
