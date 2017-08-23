cd5bbc6677e9f51f83b4ed707fe9c553efbc6f50
John Downs <john.downs@kloud.com.au>
2017-08-24 08:05:52 +1000

    Add shell solution and MVC project

---
+++ src/CatsAndOwners/CatsAndOwners/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CatsAndOwners
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
