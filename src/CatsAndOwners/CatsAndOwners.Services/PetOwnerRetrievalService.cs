1d1af5470d4bf379a6073d8510f571575cf69ff2
John Downs <john.downs@kloud.com.au>
2017-08-24 09:20:39 +1000

    Add partial implementation of GetPetOwnersAsync

--- src/CatsAndOwners/CatsAndOwners.Services/PetOwnerRetrievalService.cs
+++ src/CatsAndOwners/CatsAndOwners.Services/PetOwnerRetrievalService.cs
@@ -18,7 +18,10 @@ namespace CatsAndOwners.Services
 
         public async Task<IList<Owner>> GetPetsOwnersAsync()
         {
-            throw new NotImplementedException();
+            var result = await _httpClient.GetAsync("http://agl-developer-test.azurewebsites.net/people.json");
+            var json = await result.Content.ReadAsStringAsync();
+
+            return null;
         }
     }
 }
