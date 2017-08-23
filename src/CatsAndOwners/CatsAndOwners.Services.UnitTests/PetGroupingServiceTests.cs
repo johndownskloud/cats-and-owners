9f785280098154f2a581fb660c52570ffab389f7
John Downs <john.downs@kloud.com.au>
2017-08-24 08:53:35 +1000

    Implement more test cases

--- src/CatsAndOwners/CatsAndOwners.Services.UnitTests/PetGroupingServiceTests.cs
+++ src/CatsAndOwners/CatsAndOwners.Services.UnitTests/PetGroupingServiceTests.cs
@@ -12,13 +12,14 @@ namespace CatsAndOwners.Services.UnitTests
         private readonly PetGroupingService _petGroupingService = new PetGroupingService();
 
         [TestMethod]
-        public void GetPetNamesByOwner_WithSingleOwnerAndPet_ReturnsGenderAndPetName()
+        public void GetPetNamesByOwner_WithSingleOwnerAndSingleCat_ReturnsGenderAndCatName()
         {
             // ARRANGE
-            var catName = "Otis";
+            const string catName = "Otis";
             var owners = new List<Owner>
             {
-                new Owner {
+                new Owner
+                {
                     Gender = Gender.Male,
                     Pets = new List<Pet>
                     {
@@ -37,26 +38,108 @@ namespace CatsAndOwners.Services.UnitTests
             Assert.AreEqual(catName, result.Single().Value.Single());
         }
 
-        // TODO test cases for non-cats
+        [TestMethod]
+        public void GetPetNamesByOwner_WithSingleOwnerAndDog_ReturnsEmptyDictionary()
+        {
+            // ARRANGE
+            var owners = new List<Owner>
+            {
+                new Owner
+                {
+                    Gender = Gender.Male,
+                    Pets = new List<Pet>
+                    {
+                        new Pet { Name = "Fido", Type = PetType.Dog }
+                    }
+                }
+            };
+
+
+            // ACT
+            var result = _petGroupingService.GetPetNamesByOwner(owners);
+
+
+            // ASSERT
+            Assert.AreEqual(0, result.Count);
+        }
+        
 
         [TestMethod]
-        public void GetPetNamesByOwner_WithMultipleOwnersAndSinglePet_ReturnsGendersAndPetName()
+        public void GetPetNamesByOwner_WithMultipleMaleOwnersAndSingleCatEach_ReturnsGendersAndCatName()
         {
+            // ARRANGE
+            const string catName1 = "Otis";
+            const string catName2 = "Basil";
+            var owners = new List<Owner>
+            {
+                new Owner
+                {
+                    Gender = Gender.Male,
+                    Pets = new List<Pet>
+                    {
+                        new Pet { Name = catName1, Type = PetType.Cat }
+                    }
+                },
+                new Owner
+                {
+                    Gender = Gender.Male,
+                    Pets = new List<Pet>
+                    {
+                        new Pet { Name = catName2, Type = PetType.Cat }
+                    }
+                }
+            };
 
+
+            // ACT
+            var result = _petGroupingService.GetPetNamesByOwner(owners);
+
+
+            // ASSERT
+            Assert.AreEqual(Gender.Male, result.Single().Key);
+            Assert.AreEqual(catName1, result.Single().Value.First());
+            Assert.AreEqual(catName2, result.Single().Value.Skip(1).First());
         }
 
         [TestMethod]
-        public void GetPetNamesByOwner_WithMultipleOwnersAndMultiplePets_ReturnsGendersAndPetName()
+        public void GetPetNamesByOwner_WithMultipleOwnersAndMultiplePets_ReturnsGendersAndCatsNames()
         {
+            // ARRANGE
+            const string catName1 = "Otis";
+            const string catName2 = "Basil";
+            var owners = new List<Owner>
+            {
+                new Owner
+                {
+                    Gender = Gender.Male,
+                    Pets = new List<Pet>
+                    {
+                        new Pet { Name = catName1, Type = PetType.Cat },
+                        new Pet { Name = "Fido", Type = PetType.Dog }
+                    }
+                },
+                new Owner
+                {
+                    Gender = Gender.Male,
+                    Pets = new List<Pet>
+                    {
+                        new Pet { Name = catName2, Type = PetType.Cat },
+                        new Pet { Name = "Nemo", Type = PetType.Fish }
+                    }
+                }
+            };
 
+
+            // ACT
+            var result = _petGroupingService.GetPetNamesByOwner(owners);
+
+
+            // ASSERT
+            Assert.AreEqual(Gender.Male, result.Single().Key);
+            Assert.AreEqual(catName1, result.Single().Value.First());
+            Assert.AreEqual(catName2, result.Single().Value.Skip(1).First());
         }
-
-        [TestMethod]
-        public void GetPetNamesByOwner_WithSingleOwnerWithNullPetName_ReturnsEmptyDictionary()
-        {
-
-        }
-
+        
         [TestMethod]
         public void GetPetNamesByOwner_WithNullOwnersList_ReturnsEmptyDictionary()
         {
