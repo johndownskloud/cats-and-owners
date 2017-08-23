64d99153b18a776087e154168aaccb461c8c2b99
John Downs <john.downs@kloud.com.au>
2017-08-24 08:09:51 +1000

    Add Autofac and set up DI

--- src/CatsAndOwners/CatsAndOwners/Global.asax.cs
+++ src/CatsAndOwners/CatsAndOwners/Global.asax.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace CatsAndOwners
 {
@@ -16,6 +14,22 @@ namespace CatsAndOwners
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            InitialiseDependencyInjection();
+        }
+
+        private void InitialiseDependencyInjection()
+        {
+            var builder = new ContainerBuilder();
+
+            // add MVC controllers so Autofac recognises them
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // TODO register services here
+
+            // build the container and set it to be the default resolver
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
