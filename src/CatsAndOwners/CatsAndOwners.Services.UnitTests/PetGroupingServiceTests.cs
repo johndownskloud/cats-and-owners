b44be01690bf6567e117132fa1b6c343d65a006e
John Downs <john.downs@kloud.com.au>
2017-08-24 08:27:00 +1000

    Add failing tests for edge cases

--- src/CatsAndOwners/CatsAndOwners.Services.UnitTests/PetGroupingServiceTests.cs
+++ src/CatsAndOwners/CatsAndOwners.Services.UnitTests/PetGroupingServiceTests.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Collections;
+using System.Collections.Generic;
+using CatsAndOwners.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CatsAndOwners.Services.UnitTests
 {
     [TestClass]
     public class PetGroupingServiceTests
     {
+        private PetGroupingService _petGroupingService;
+
         [TestMethod]
         public void GetPetNamesByOwner_WithSingleOwnerAndPet_ReturnsGenderAndPetName()
         {
-            
         }
 
         [TestMethod]
@@ -32,13 +36,27 @@ namespace CatsAndOwners.Services.UnitTests
         [TestMethod]
         public void GetPetNamesByOwner_WithNullOwnersList_ReturnsEmptyDictionary()
         {
+            // ACT
+            var result = _petGroupingService.GetPetNamesByOwner(null);
 
+
+            // ASSERT
+            Assert.AreEqual(0, result.Count);
         }
 
         [TestMethod]
         public void GetPetNamesByOwner_WithEmptyOwnersList_ReturnsEmptyDictionary()
         {
-            
+            // ARRANGE
+            var owners = new List<Owner>();
+
+
+            // ACT
+            var result = _petGroupingService.GetPetNamesByOwner(owners);
+
+
+            // ASSERT
+            Assert.AreEqual(0, result.Count);
         }
     }
 }
